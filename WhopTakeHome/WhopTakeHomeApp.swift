64ea97301d06cbfd8da848515985ac5e81e10cf7
Stephen Sowole <stephensowole@gmail.com>
2024-12-04 19:57:12 -0800

    Initial Commit

---
+++ WhopTakeHome/WhopTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhopTakeHomeApp.swift
+//  WhopTakeHome
+//
+//  Created by Stephen Sowole on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WhopTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhopTakeHomeTests/WhopTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhopTakeHomeTests.swift
+//  WhopTakeHomeTests
+//
+//  Created by Stephen Sowole on 12/4/24.
+//
+
+import Testing
+@testable import WhopTakeHome
+
+struct WhopTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
