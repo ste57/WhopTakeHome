06b6f0552b8c7d919b53931a40705af754812ee5
Stephen Sowole <stephensowole@gmail.com>
2024-12-10 16:28:01 -0800

    Create Strings.swift

---
+++ WhopTakeHome/Utility/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Strings.swift
+//  WhopTakeHome
+//
+//  Created by Stephen Sowole on 12/10/24.
+//
+
+import Foundation
+
+public class Strings {
+
+    static var folder: String {
+        return NSLocalizedString("Folder", comment: "")
+    }
+}
