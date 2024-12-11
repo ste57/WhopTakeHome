aa3b6829c0add26918e43eb3041862d1da8be2cf
Stephen Sowole <stephensowole@gmail.com>
2024-12-10 16:27:57 -0800

    Add SystemImage for convenience

---
+++ WhopTakeHome/Utility/SystemImage.swift
@@ -0,0 +1,15 @@
+//
+//  SystemImage.swift
+//  WhopTakeHome
+//
+//  Created by Stephen Sowole on 12/10/24.
+//
+
+enum SystemImage: String {
+    case link
+    case folder
+
+    var systemName: String {
+        rawValue
+    }
+}
